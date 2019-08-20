7c09631ea0587c36c65b38e3d420afb242c94e13
Jelena <jelena.zobenica1@gmail.com>
2019-08-20 20:47:57 +0200

    Add video_details component and functionality to display selected video

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetails from './components/video_details';
 
 const API_KEY = 'AIzaSyBcJubKFrKk0rEkoVNJ9YWn9jYGoMG_dBI';
 
@@ -10,18 +11,27 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       // this.setState({ videos: videos });
       // case when key and value have same name:
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetails video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
